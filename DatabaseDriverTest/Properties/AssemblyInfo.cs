c460fef542b870b09427ada05f70722dd182bbb2
tokyo2mogadish <najboljiodsvih@gmail.com>
2020-01-26 20:58:02 +0100

    nije uhvatio test

---
+++ DatabaseDriverTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DatabaseDriverTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatabaseDriverTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("274635ad-2e9d-4315-9f90-fa05f52c25dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DatabaseDriverTest/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheShop;
+using System.Collections.Generic;
+
+namespace DatabaseDriverTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        DatabaseDriver databaseDriver = new DatabaseDriver();
+        [TestMethod]
+        public void GetById()
+        {
+            Article article = new Article();
+            try
+            {
+                //act
+                article = databaseDriver.GetById(1);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Couldnt fetch an article: \n" + e.Message);
+            }
+            Assert.AreEqual("Article from supplier1_1", article.Name_of_article, "Retrieved value doesnt match expected one.");
+        }
+        [TestMethod]
+        public void GetAllArticles()
+        {
+            //arrange
+            List<Article> articles = new List<Article>();
+            //act
+            try
+            {
+                articles = databaseDriver.GetAllArticles();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Couldnt retrieve list of articles:\n" + ex.Message);
+            }
+            //assert
+            Assert.IsNotNull(articles, "List of articles does not exists");
+            Assert.AreEqual(9, articles.Count, "Retrieved number of rows is not as expected");
+            Assert.AreEqual("Article from supplier1_3", articles[2].Name_of_article, "Retrieved value does not match excpected one");
+
+        }
+        [TestMethod]
+        public void Save()
+        {
+            //arrange
+            List<Article> articles = new List<Article>();
+            try
+            {
+                articles = databaseDriver.GetAllArticles();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Couldnt retrieve list of articles:\n" + ex.Message);
+            }
+            Article article = new Article()
+            {
+                ID = 1,
+                Name_of_article = "Article from supplier4_1",
+                ArticlePrice = 58,
+                IsSold = false
+            };
+            //act 
+            try
+            {
+                databaseDriver.Save(article);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Couldnt save article:\n" + ex.Message);
+            }
+            //assert
+            Assert.AreEqual("Article from supplier4_1", articles[9].Name_of_article, "Retrieved value does not match excpected one");
+
+        }
+    }
+}
