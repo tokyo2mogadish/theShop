ae0c603b6e3c65279b948772ae8a508100e7af30
tokyo2mogadish <najboljiodsvih@gmail.com>
2020-01-26 20:50:33 +0100

    izdvojene  klase u nove fajlove; dodat unit test za metode databasedriver klase

---
+++ TheShop/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheShop
+{
+    public class Logger
+    {
+        public static void Info(string message)
+        {
+            Console.WriteLine("Loger Info: " + message);
+        }
+
+        public static void Error(string message)
+        {
+            Console.WriteLine("Loger Error: " + message);
+        }
+
+        public static void Debug(string message)
+        {
+            Console.WriteLine("Loger Debug: " + message);
+        }
+    }
+}
